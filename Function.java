7207ce964c003cc2770d8c2fcdda41c6a1372f06
keshavpoilk369-tech <keshavpoilk369@gmail.com>
2026-09-23 18:42:11 +0530

    Add files via upload

--- Function.java
+++ Function.java
@@ -1,18 +1,44 @@
+package Fun124;
 
-package Aimaster;
-import java.util.Scanner;
 public class Function {
-    public static void PrintMyName(String name){
-        System.out.println(name);
-        return;
+    static void main() {
+        // Function in java
+
+        name();
+//        String name = "keshav";
+//        sayhello(name);
+
+        sayhello("keshav");
+
+//        int x = getnum();
+//        System.out.println(x);
+        System.out.println(getnum());
+
+//       int x = multiply(5, 6);
+//        System.out.println(x);
+
+        System.out.println(multiply(4 , 2));
+        }
+
+        // no input , no output
+    static void name() {
+        System.out.println("Hello");
+        return;  // optional
+    }
+    // input , not output
+
+    static void sayhello(String name){
+        System.out.println("hello "+ name);
+    }
+    // no input , output
+    static int getnum(){
+        return 10;
     }
 
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        String name = sc.nextLine();
-
-        PrintMyName(name); //__>> call kiya function ko
-
+    // input , output
+    static int multiply(int a , int b){
+        int mul = a * b;
+        return mul;  // tow line will be reduse in one line -->> return (a * b );
     }
-}
+    }
+
